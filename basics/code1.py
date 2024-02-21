e963a9a00ac0038bfaa4925fd0bc3e13cd63d661
Yashwanth B M <yashwanthbm36@gmail.com>
2024-02-21 15:19:17 +0530

    learnt datatypes

---
+++ basics/code1.py
@@ -0,0 +1,20 @@
+# object types/data types :-
+# Numbers : 1234, 3.1415, 3+4j, 0b111, Decimal(), Fraction ()
+# String : 'spam', b'a\x01c'
+# List : [1,2,3,5,"three",2.43], list(range(10))
+# Tuple : (1,2,'spam',4,'U')
+# Dict : {'food' : 'spam'}, dict(hours=10)
+# Set : set('abc'), {'a','b','c'}
+# File : open('eggs.text')
+# Boolean : True, False
+# None
+# Functions, Modules, Classes
+# Advanced : Decorators, Generators, Iterators, MetaProgramming
+
+import math
+print("pi value : ",math.pi)
+
+yourName = 'yashwanth'
+
+print("[:-2]",yourName[:-2])
+
